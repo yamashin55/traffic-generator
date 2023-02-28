f0632933fa8197f23bd3e3eec471fd4b591b99ef
yamashin55 <g.shingo.001@gmail.com>
2023-02-28 22:31:37 +0900

    modified:   baseline_menu.sh

--- baseline_menu.sh
+++ baseline_menu.sh
@@ -7,11 +7,11 @@ clear
 echo "Traffic Baselining"
 echo
 IP=$1
-#IP="10.1.10.61"
+IP="10.15.2.98"
 if [ "$IP" == "" ]
  then
-        #echo -n "Enter Target IP as an Argument"
-        #exit
+        echo -n "Enter Target IP as an Argument"
+        exit
 fi
 
 BASELINE='Please enter your type of baslining: '
@@ -26,9 +26,9 @@ do
                                 echo
 				for i in $(eval echo "{0..`date +%M`}")
                                         do
-                                                curl -0 --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
-                                                curl -0 --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-                                                curl -0 --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
+                                                curl -0 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
+                                                curl -0 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
+                                                curl -0 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
                                         done
                                 #sleep 0.1
                         done    
@@ -42,17 +42,17 @@ do
                                 if (( `date +%k` % 2 )); then
                                         for i in {1..100};
                                                 do
-                                                        curl -0 --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
-                                                        curl -0 --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-                                                        curl -0 --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
+                                                        curl -0 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
+                                                        curl -0 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
+                                                        curl -0 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
 #                                                	curl -0 -s -o /dev/null -A "`sort -R ./source/useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`sort -R ./source/urls.txt | head -1`
                                                 done
                                 else
                                         for i in {1..50};
                                                 do
-                                                        curl --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
-                                                        curl --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-                                                        curl --interface 10.1.10.200 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
+                                                        curl -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
+                                                        curl -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
+                                                        curl -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
                                                 done
                                 fi
                                 #sleep 0.1
